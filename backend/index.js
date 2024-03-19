f29440d182d0af8a7007ef77045a6eb1d7d4fc0c
sachinpalsp917 <106242320+sachinpalsp917@users.noreply.github.com>
2024-03-19 10:58:58 +0000

    create database and connected it!!!!!

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 import express from "express";
+import "dotenv/config";
+import  connectDB  from "./src/db/db.js";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello world !@!!!");
-});
-
-app.listen(3000, (req, res) => {
-  console.log("Server running");
-});
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, (req, res) => {
+      console.log("Server running on PORT: ", process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed: ", err);
+  });
