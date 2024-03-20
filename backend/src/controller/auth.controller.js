9439f2b977d62e4fc1f28b557913a0a8c570452c
sachinpalsp917 <106242320+sachinpalsp917@users.noreply.github.com>
2024-03-20 05:21:11 +0000

    create the signin api route

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../model/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/apiError.js";
+import jwt from "jsonwebtoken";
 
 const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -29,4 +30,37 @@ const signup = async (req, res, next) => {
   }
 };
 
-export { signup };
+const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All fields are required"));
+  }
+
+  try {
+    //checking the username
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    // checking the password
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid Password"));
+    }
+
+    //authenticating the user
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    //seperate the password from the data sent
+    const { password: pass, ...userData } = validUser._doc;
+    res
+      .status(200)
+      .cookie("access-token", token, { httpOnly: true })
+      .json(userData);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { signup, signin };
