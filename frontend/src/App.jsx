55c7332451d5c043ffb1d9cbbd492a0319b8a67c
sachinpalsp917 <106242320+sachinpalsp917@users.noreply.github.com>
2024-03-20 05:09:30 +0000

    create footer section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Train from "./pages/Train";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
@@ -20,5 +21,6 @@ export default function App() {
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
     </Routes>
+    <Footer/>
   </BrowserRouter>);
 }
