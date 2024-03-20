f84d2d655ce49126e645b8f6a053d0962f6b8130
sachinpalsp917 <106242320+sachinpalsp917@users.noreply.github.com>
2024-03-20 10:22:53 +0000

    make the dashboard private

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,19 +8,23 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
-  <BrowserRouter>
-  <Header/>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/train" element={<Train />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
-    </Routes>
-    <Footer/>
-  </BrowserRouter>);
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route path="/train" element={<Train />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
